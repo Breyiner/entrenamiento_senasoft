8e0285657dd914ad7c79d7590dde55787ce71c1e
Breyiner <breyneracosta7@gmail.com>
2025-10-11 15:14:56 -0500

    se implementa autenticación con login y logout y sus validaciones

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'document' => 'required|min:6|max:10',
+      'password' => 'required|string|min:8|max:20',
+    ];
+  }
+
+  /**
+
+   * Get the error messages for the defined validation rules.
+   *
+   * @return array
+   */
+  public function messages()
+  {
+    return [
+      'document.required' => 'El :attribute es obligatorio',
+      'password.required' => 'La :attribute es obligatoria',
+
+      'document.min' => 'El :attribute debe tener al menos :min caracteres.',
+      'document.max' => 'El :attribute no debe tener más de :max caracteres',
+      'password.min' => 'La :attribute debe tener al menos :min caracteres.',
+      'password.max' => 'La :attribute no debe tener más de :max caracteres',
+
+      'document.numeric' => 'El :attribute debe ser un número',
+      'password.string' => 'La :attribute debe ser texto',
+    ];
+  }
+
+  /**
+   * Get custom attributes for validator errors.
+   *
+   * @return array<string, string>
+   */
+  public function attributes(): array
+  {
+    return [
+      'document' => 'número de documento o NIT',
+      'password' => 'contraseña'
+    ];
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Enums\TokenAbility;
+use App\Http\Controllers\API\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(
+  function () {
+
+    Route::post('/refresh-token', [AuthController::class, 'refreshToken'])
+      ->middleware('ability:' . TokenAbility::ISSUE_ACCESS_TOKEN->value);
+
+    Route::post('/logout', [AuthController::class, 'logOut']);
+  }
+);
